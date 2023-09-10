f9f9f4b0b40f8be6bd41e3a8ad2097b86364048b
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-09-10 18:32:26 +0200

    axios general file

---
+++ client/src/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export let BASE_ENDPOINT = "";
+//BASE_ENDPOINT = 'https://api.dev.somethingserver.xyz';
+// when developing locally, change this value to local
+export const APP_ENVIRONMENT = "local";
+
+if (APP_ENVIRONMENT === "local") {
+  BASE_ENDPOINT = "http://localhost:5000";
+} /* else if (APP_ENVIRONMENT === "development") {
+  BASE_ENDPOINT = "https://ineedsomething.herokuapp.com";
+} else if (APP_ENVIRONMENT === "staging") {
+  BASE_ENDPOINT = "https://ineedsomething.herokuapp.com";
+} else if (APP_ENVIRONMENT === "production") {
+  BASE_ENDPOINT = "https://ineedsomething.herokuapp.com";
+} */
+
+const BASE_URL = `${BASE_ENDPOINT}/api/v1`;
+//console.log(BASE_ENDPOINT)
+
+export default axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json", Accept: "application/json" },
+  withCredentials: true,
+});
