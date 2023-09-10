fd3d9d88c288c7ca9b5a4092c75ef4f92a1123e7
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-09-10 21:37:35 +0200

    redux persistent cancelled

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import DocumentIcon from "./assets/svg/Document";
-import CallIcon from "./assets/svg/Call";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="dark">
+      <ToastContainer position="bottom-center" limit={1} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
--- client/src/redux/currentUserSlice.js
+++ client/src/redux/currentUserSlice.js
@@ -2,15 +2,35 @@ import { createSlice } from "@reduxjs/toolkit";
 //import Cookies from "js-cookie";
 
 const initialState = {
-  loggedUser: null,
+  loggedUser: {
+    id: "",
+    name: "hello",
+    email: "",
+    picture: "",
+    status: "",
+  },
+  status: "",
+  error: "",
 };
 
 const currentUSlicer = createSlice({
   name: "currentUser",
   initialState,
-  reducers: {},
+  reducers: {
+    reduxLogout: (state, action) => {
+      state.status = "";
+      state.loggedUser = {
+        id: "",
+        name: "",
+        email: "",
+        picture: "",
+        status: "",
+      };
+    },
+    reduxRegisterUser: (state, action) => {},
+  },
 });
 
-export const {} = currentUSlicer.actions;
+export const { reduxLogout, reduxRegisterUser } = currentUSlicer.actions;
 
 export default currentUSlicer.reducer;
