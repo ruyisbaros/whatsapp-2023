ae26056e216b10c7185bef1681235dd74a27a921
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-09-10 19:07:45 +0200

    tailwind css set

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import DocumentIcon from "./assets/svg/Document";
+import CallIcon from "./assets/svg/Call";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
 };
 
 export default App;
